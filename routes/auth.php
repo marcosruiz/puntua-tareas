50e9347fae35152c4595a87363d97c38a0212f1e
Marcos Ruiz <marcosruizgarcia@gmail.com>
2025-05-21 14:55:11 +0200

    wip min 13:58

--- routes/auth.php
+++ routes/auth.php
@@ -8,6 +8,10 @@ use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\QuestionGroupController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\OptionController;
+use App\Http\Controllers\ResponseController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
